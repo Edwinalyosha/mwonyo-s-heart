1c3f638c87076dcac7516c724aecf50bebe3d7f7
Edwinalyosha <98754274+Edwinalyosha@users.noreply.github.com>
2024-05-24 11:23:42 +0300

    Add files via upload

---
+++ sr.js
@@ -0,0 +1,28 @@
+ScrollReveal().reveal('.sr-left', {origin:'left',delay:700, distance:'70px', reset:true, duration:600});
+ScrollReveal().reveal('.sr-left0', {origin:'left',delay:700, distance:'70px', reset:false, duration:600});
+
+ScrollReveal().reveal('.sr-right',{origin:'right', delay:700, distance:'70px', reset:true, duration:600});
+
+ScrollReveal().reveal('.sr-top',{origin:'top', delay:700, distance:'70px', reset:true, duration:600});
+
+ScrollReveal().reveal('.sr-bottom',{origin:'bottom', delay:700, distance:'70px', reset:true, duration:600});
+
+ScrollReveal().reveal('.sr-fade',{ delay:500, reset:true , duration:600});
+
+
+ScrollReveal().reveal('.sr-left1', {origin:'left',delay:350, distance:'70px', reset:true});
+
+ScrollReveal().reveal('.sr-right1',{origin:'right', delay:350, distance:'70px', reset:true});
+
+ScrollReveal().reveal('.sr-top1',{origin:'top', delay:350, distance:'70px', reset:true});
+
+ScrollReveal().reveal('.sr-bottom1',{origin:'bottom', delay:350, distance:'70px', reset:true});
+ScrollReveal().reveal('.sr-bottom1_1',{origin:'bottom', delay:450, distance:'70px', reset:true});
+ScrollReveal().reveal('.sr-bottom1_2',{origin:'bottom', delay:550, distance:'70px', reset:true});
+
+ScrollReveal().reveal('.sr-fade1',{ delay:250, reset:true});
+ScrollReveal().reveal('.sr-fade1_1',{ delay:300, reset:true});
+ScrollReveal().reveal('.sr-fade1_2',{ delay:400, reset:true});
+ScrollReveal().reveal('.sr-fade1_3',{ delay:500, reset:true});
+
+ScrollReveal().reveal('.sr-bottom2',{origin:'bottom', delay:1050, distance:'70px', reset:true, duration:600});
